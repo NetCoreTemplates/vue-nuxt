accc2f355a6f8f72c5afe9e2f6781b98ce5b2085
Demis Bellot <demis.bellot@gmail.com>
2019-06-26 01:24:48 -0400

    Use latest modular auth-memory AuthRepository

--- MyApp/Configure.Auth.cs
+++ MyApp/Configure.Auth.cs
@@ -1,41 +1,10 @@
 using Microsoft.Extensions.DependencyInjection;
-using System.Collections.Generic;
-using Funq;
 using ServiceStack;
 using ServiceStack.Auth;
-using ServiceStack.Caching;
-using ServiceStack.Configuration;
 using ServiceStack.FluentValidation;
 
 namespace MyApp
 {
-    [Priority(-100)] // Run before ConfigureAuthRepository
-    public class ConfigureAuth : IConfigureAppHost, IConfigureServices
-    {
-        public void Configure(IServiceCollection services)
-        {
-            //services.AddSingleton<ICacheClient>(new MemoryCacheClient()); //Store User Sessions in Memory Cache (default)
-        }
-
-        public void Configure(IAppHost appHost)
-        {
-            var AppSettings = appHost.AppSettings;
-            appHost.Plugins.Add(new AuthFeature(() => new CustomUserSession(),
-                new IAuthProvider[] {
-                    new JwtAuthProvider(AppSettings) { AuthKey = AesUtils.CreateKey() }, // Use persistent key to enable JWT's beyond restarts
-                    new CredentialsAuthProvider(AppSettings),     /* Sign In with Username / Password credentials */
-                    new FacebookAuthProvider(AppSettings),        /* Create App https://developers.facebook.com/apps */
-                    new GoogleAuthProvider(AppSettings),          /* Create App https://console.developers.google.com/apis/credentials */
-                    new MicrosoftGraphAuthProvider(AppSettings),  /* Create App https://apps.dev.microsoft.com */
-                }));
-
-            appHost.Plugins.Add(new RegistrationFeature()); //Enable /register Service
-
-            //override the default registration validation with your own custom implementation
-            appHost.RegisterAs<CustomRegistrationValidator, IValidator<Register>>();
-        }
-    }
-    
     // Add any additional metadata properties you want to store in the Users Typed Session
     public class CustomUserSession : AuthUserSession 
     {
@@ -51,6 +20,31 @@ namespace MyApp
                 RuleFor(x => x.DisplayName).NotEmpty();
                 RuleFor(x => x.ConfirmPassword).NotEmpty();
             });
+        }
+    }
+
+    public class ConfigureAuth : IConfigureAppHost, IConfigureServices
+    {
+        public void Configure(IServiceCollection services)
+        {
+            //services.AddSingleton<ICacheClient>(new MemoryCacheClient()); //Store User Sessions in Memory Cache (default)
+        }
+
+        public void Configure(IAppHost appHost)
+        {
+            var AppSettings = appHost.AppSettings;
+            appHost.Plugins.Add(new AuthFeature(() => new CustomUserSession(),
+                new IAuthProvider[] {
+                    new CredentialsAuthProvider(AppSettings),     /* Sign In with Username / Password credentials */
+                    new FacebookAuthProvider(AppSettings),        /* Create App https://developers.facebook.com/apps */
+                    new GoogleAuthProvider(AppSettings),          /* Create App https://console.developers.google.com/apis/credentials */
+                    new MicrosoftGraphAuthProvider(AppSettings),  /* Create App https://apps.dev.microsoft.com */
+                }));
+
+            appHost.Plugins.Add(new RegistrationFeature()); //Enable /register Service
+
+            //override the default registration validation with your own custom implementation
+            appHost.RegisterAs<CustomRegistrationValidator, IValidator<Register>>();
         }
     }
 }
