7910f6d7157a9898721eedeba2786fe82d8944aa
Demis Bellot <demis.bellot@gmail.com>
2021-06-23 00:27:41 +0800

    Update AuthRepo

--- MyApp/Configure.AuthRepository.cs
+++ MyApp/Configure.AuthRepository.cs
@@ -46,7 +46,7 @@ namespace MyApp
             var authRepo = appHost.Resolve<IAuthRepository>();
             authRepo.InitSchema();
 
-            CreateUser(authRepo, "admin@email.com", "Admin User", "p@55wOrd", roles:new[]{ RoleNames.Admin });
+            //CreateUser(authRepo, "admin@email.com", "Admin User", "p@55wOrd", roles:new[]{ RoleNames.Admin });
         }
 
         public void BeforePluginsLoaded(IAppHost appHost)
